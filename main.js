43df24c67f2d8346d0a0acdbe7dfa0cae59a1deb
rushiah <tomlmaooxd@gmail.com>
2022-02-03 01:15:20 +0000

    UI

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 940,
+    minHeight: 560,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      devTools: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
