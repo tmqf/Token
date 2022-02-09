ac98d6b4d1a4c0f00a22f38313d3b8e71bf39f7a
rushiah <tomlmaooxd@gmail.com>
2022-02-09 10:47:30 +0000

    fixed home page and other problems

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,14 @@ def createApp():
 
     createDB(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def createDB(app):
