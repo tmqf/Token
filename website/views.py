0e8df3010e76a5f7ab3a79219b6f159a9c76c839
rushiah <tomlmaooxd@gmail.com>
2022-02-18 00:46:13 +0000

    testing

--- website/views.py
+++ website/views.py
@@ -4,10 +4,6 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
-def default():
-    return render_template('login.html', user=current_user)
-
-@views.route('/home')
 @login_required
 def home():
     return render_template('home.html', user=current_user)
