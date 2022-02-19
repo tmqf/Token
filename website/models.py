b719dd2fc402d54f1a138f2dbe00203f0005b091
Tom <tommmxddd@gmail.com>
2022-02-19 01:48:31 +0000

    new branch

--- website/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from flask_login import UserMixin
-from sqlalchemy.sql import func
-
-class Person(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(250))
-    username = db.Column(db.String(32))
