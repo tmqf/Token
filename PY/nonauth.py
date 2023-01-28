52ba0bddb27394418d6c945833581c580ba38c62
tomosus <67664316+tomosus@users.noreply.github.com>
2023-01-28 11:11:27 +0000

    Delete PY directory

--- PY/nonauth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, url_for, redirect, request
-from flask_login import login_required, current_user
-
-noauth = Blueprint('noauth', __name__)
-
-@noauth.route('/')
-def home():
-    return render_template('home.html')
